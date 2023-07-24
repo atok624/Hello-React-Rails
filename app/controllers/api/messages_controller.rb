3d610f6c386c4543520369510bac10f454b627a7
atok624 <84607674+atok624@users.noreply.github.com>
2023-07-24 22:33:20 +0000

    Fix linter errors

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Api
   class MessagesController < ApplicationController
     def random_greeting
@@ -6,4 +8,3 @@ module Api
     end
   end
 end
-
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'full_name returns the concatenated first name and last name' do
+    user = User.new(first_name: 'John', last_name: 'Doe')
+    assert_equal 'John Doe', user.full_name
+  end
 end
